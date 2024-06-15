16847f08c5182272c9e2d8046cbb5bbb3148f7b8
Tad-z <david.akintade1000@gmail.com>
2024-06-15 22:21:42 +0100

    auth

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const app = express();
 const main = require("./models/db");
 const userRouter = require("./routes/user");
 const planRouter = require("./routes/plan");
+const cors = require("cors")
 require("dotenv").config();
+
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(cors({
+    origin: '*', // Allows all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allows specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allows specific headers
+    credentials: true // Allows credentials (optional)
+  }));
 
 
 main()
